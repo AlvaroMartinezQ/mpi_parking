8200c9b72f8066f42c95e764d65669d35bd2de93
alvaro <alvaroo2302@gmail.com>
2020-11-24 13:00:26 +0100

    fix: get parameters in function

--- orquestador_parking.c
+++ orquestador_parking.c
@@ -21,23 +21,38 @@
 
 
 // Funciones
-void imprime_plazas(int plantas, int plazas, int ** parking);
-int** init_parking(int plantas, int plazas);
+
 
 int main(int argc, char* argv[]){
     // Declaracion de variables antes de iniciar
-    int plazas = strtol(argv[1], NULL, 10);
-    int plantas = strtol(argv[2], NULL, 10);
+    int plazas = atoi(argv[1]);
+    int plantas = atoi(argv[2]);
 
     printf("Plantas: %d, con %d plazas por planta.\n", plantas, plazas);
 
-    int ** parking = init_parking(plantas, plazas);
+    int parking[plantas][plazas];
+    int i, j, k;
+    int asignado = 0;
+    for(i=0; i < plantas; i++){
+        for(j=0; j < plazas; j++){
+            parking[i][j] = 0; // 0 sera libre y cualquier otro numero aparcado [id del proceso que pide plaza]
+        }
+    }
+
+    printf("Parking inicializado.\n");
 
     MPI_Status status;
 
     int aparcado = -1;
 
-    imprime_plazas(plantas, plazas, parking);
+    printf("Estado actual del parking: \n");
+    for(i=0; i < plantas; i++){
+        printf("-> Planta %d: ", i);
+        for(j=0; j < plazas; j++){
+            printf("[%d]", parking[i][j]);
+        }
+        printf("\n");
+    }
 
     // Inicializacion MPI
     MPI_Init(&argc, &argv);
@@ -56,20 +71,21 @@ int main(int argc, char* argv[]){
         // Realizar comparaciones necesarias, primero tipo de operacion, despues tipo de vehiculo.
         if(recibido[1] == 0){ // Quiero entrar
             if(recibido[2] == 0) { // Soy un coche
-                int i, j;
                 int posicion[2]; // [planta][plaza]
                 posicion[0] = -1;
                 posicion[1] = -1;
                 for(i=0; i < plantas; i++){
                     for(j=0; j < plazas; j++){
-                        if(parking[i][j] == 0){
+                        if(parking[i][j] == 0 && asignado == 0){
                             // Aqui somos capaces de asignar plaza
-                            parking[i][j] = recibido[0]; // Se pudo asignar
+                            parking[i][j] = recibido[0];
                             posicion[0] = i;
                             posicion[1] = j;
+                            asignado = 1; // Se pudo asignar
                         }
                     }
-                }// No se pudo asignar
+                }
+                asignado = 0;
                 if(posicion[0] != -1){ // Se ha asignado
                     printf("Coche %d aparcado en la planta %d, en la plaza %d.\n", recibido[0], posicion[0], posicion[1]);
                     aparcado = 1;
@@ -80,7 +96,6 @@ int main(int argc, char* argv[]){
                     MPI_Send(&aparcado, 1, MPI_INT, recibido[0], 0, MPI_COMM_WORLD);
                 }
             } else if (recibido[2] == 1) { // Soy un camion
-                int i, j, k;
                 int posicion[4]; // [planta][plaza][planta][plaza]
                 posicion[0] = -1;
                 posicion[1] = -1;
@@ -90,21 +105,23 @@ int main(int argc, char* argv[]){
                     for(j=0; j < plazas; j++){
                         k = j;
                         k++;
-                        if(k < plazas){ // Un camion debe de aparcar en dos huecos contigos
-                            if(parking[i][j] == 0 && parking[i][k] == 0){
+                        if(k < plazas){ // Un camion debe de aparcar en dos huecos contiguos
+                            if(parking[i][j] == 0 && parking[i][k] == 0 && asignado == 0){
                                 parking[i][j] = recibido[0];
                                 parking[i][k] = recibido[0];
 
                                 posicion[0] = i;
                                 posicion[1] = j;
                                 posicion[2] = i;
-                                posicion[3] = k; // Se pudo asignar
+                                posicion[3] = k;
+                                asignado = 1; // Se pudo asignar
                             }
                         } else {
                             k = 0;
                         }
                     }
-                }// No se pudo asignar
+                }
+                asignado = 0;
                 if(posicion[0] != -1){ // Se ha asignado
                     printf("Camion %d aparcado en la planta %d, en las plazas %d y %d.\n", recibido[0], posicion[0], posicion[1], posicion[3]);
                     aparcado = 1;
@@ -118,7 +135,6 @@ int main(int argc, char* argv[]){
         } else if(recibido[1] == 1) { // Quiero salir
             if(recibido[2] == 0) { // Soy un coche
                 int salida; // salida = 0 -> correcto, EOC -> error
-                int i, j;
                 for(i=0; i < plantas; i++){
                     for(j=0; j < plazas; j++){
                         if(parking[i][j] == recibido[0]) {
@@ -131,7 +147,6 @@ int main(int argc, char* argv[]){
                 salida = -1; // No encontrado
             } else if (recibido[2] == 1) { // Soy un camion
                 int salida; // salida = 0 -> correcto, EOC -> error
-                int i, j; 
                 int k = 0;
                 for(i=0; i < plantas; i++){
                     for(j=0; j < plazas; j++){
@@ -149,32 +164,16 @@ int main(int argc, char* argv[]){
                 salida = -1; // No encontrado
             }
         }
-        imprime_plazas(plantas, plazas, parking);
+        printf("Estado actual del parking: \n");
+        for(i=0; i < plantas; i++){
+            printf("-> Planta %d: ", i);
+            for(j=0; j < plazas; j++){
+                printf("[%d]", parking[i][j]);
+            }
+            printf("\n");
+        }
         sleep(1); // Para evitar mucha repeticion de imprimir el parking
     }
     // Fin
     MPI_Finalize();
-}
-
-void imprime_plazas(int plantas, int plazas, int ** parking){
-    printf("Estado actual del parking: \n");
-    int i, j;
-    for(i=0; i < plantas; i++){
-        printf("-> Planta %d: ", i);
-        for(j=0; j < plazas; j++){
-            printf("[%d]", parking[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int** init_parking(int plantas, int plazas){
-    int** parking;
-    int i, j;
-    for(i=0; i < plantas; i++){
-        for(j=0; j < plazas; j++){
-            parking[i][j] = 0; // 0 sera libre y cualquier otro numero aparcado [id]
-        }
-    }
-    return parking;
 }
