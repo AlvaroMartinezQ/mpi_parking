2620f626f6facce420157f8a3e86dd186769f1e2
alvaro <alvaroo2302@gmail.com>
2020-11-24 10:37:34 +0100

    feature: main car idea

--- coche.c
+++ coche.c
@@ -6,9 +6,25 @@
 
 /*
     Un coche ocupa 1 espacio en el parking
+    Necesito: 
+    - Id del proceso o algo parecido que lo identifique
+    - Operacion a realizar, primero sera entrada, luego salida
+    - Tipo de vehiculo, en este caso no cambiara -> coche
 */
 
 int main(int argc, char* argv[]){
+    // Declaracion de variables antes de iniciar
+    int recibido;
+    int enviado[3]; // [id hilo][operacion][tipo de vehiculo]
+    MPI_Status status;
 
-    return 0;
+    // Inicializacion MPI
+    MPI_Init(&argc, &argv);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // TODO
+
+    // Fin
+    MPI_Finalize();
 }
